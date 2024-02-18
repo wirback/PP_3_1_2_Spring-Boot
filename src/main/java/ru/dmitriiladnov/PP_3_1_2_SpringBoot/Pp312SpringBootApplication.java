375f15b34910dc7ed025a94753d63f641be6f65e
Dmitrii Ladnov <ladnov.dmitry@yandex.ru>
2024-02-18 19:18:32 +0300

    Init repository

---
+++ src/main/java/ru/dmitriiladnov/PP_3_1_2_SpringBoot/Pp312SpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.dmitriiladnov.PP_3_1_2_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp312SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp312SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dmitriiladnov/PP_3_1_2_SpringBoot/Pp312SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dmitriiladnov.PP_3_1_2_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp312SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
